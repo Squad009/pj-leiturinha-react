c3c9480c5a648f3c77ffd3cf4db537be593c0af6
sonival.santos <sonival.santos@os2h-alp-mac177.local>
2022-11-05 19:33:31 -0300

    atualizacao

--- src/components/leiturinha/LeiturinhaBanner.jsx
+++ src/components/leiturinha/LeiturinhaBanner.jsx
@@ -8,14 +8,12 @@ class LeiturinhaBanner extends React.Component{
         return(
             <div className="banner">
                 <div className="container banner-elements legenda">
-
                     <div>
                     <h2 className="title_font">Compartilhe e cresça!</h2>
                     <p>Faça parte dessa comunidade, descubra mundos incríveis a cada palavra lida e divida as suas histórias
                         também!
                     </p>
                     </div>
-
                     <div>
                     <Link to="criar_conto" className="btn button_app_primary mr-3"> Compartilhe sua história</Link>
                     </div>
